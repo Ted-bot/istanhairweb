92db7150de8ee2127ba01163a439cacfa5e8d95d
ted <tkbotch@gmail.com>
2021-02-12 21:12:16 +0100

    updating old files

--- youtube_api.php
+++ youtube_api.php
@@ -15,20 +15,16 @@ if (isset($_POST["search"])){
 
         header('Content-type: application/json');
         $videoList = json_decode(file_get_contents('https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId='.$channelID.'&maxResults='.$maxResults.'&q='.$search.'&key='.$API_key.''));
-        
-        if($videoList){
-            $response_array['status'] = 'success';  
-
+                    
             foreach($videoList->items as $item){
-                //Embed video
-                if(isset($item->id->videoId)){
+
                     $search = <<<DELIMETER
                         <div>
                             <div class="youtube-video" 
                                 style="display: inline-flex; position: relative;  aspect-ratio: 16/9; width:300px; max-height: 250px; border:solid 1px lightgrey;"
                                 >
                                 <iframe width="280" height="150" src="https://www.youtube.com/embed/{$item->id->videoId}" frameborder="0" allowfullscreen></iframe>
-                                <h2>'. $item->snippet->title .'</h2>
+                                <h2> {$item->snippet->title} </h2>
                             </div>
                             <a href="youtube-upload.php?select-videoId={$item->id->videoId}&video-title={$item->snippet->title}" id="getSelectedYoutubeId" style="position: absolute; z-index: 30; border-radius: 2px; border: none; background-color:blue; opacity:85%; margin-top: 5px;">
                                 <i class="fas fa-check-circle"></i>
@@ -36,15 +32,8 @@ if (isset($_POST["search"])){
                         </div>
 DELIMETER;
                 }
-                return $search;
+                echo $search;
             }
-
-        }else {
-            $response_array['status'] = 'error';  
-        }
-
-        
-    }
 
     youtubeSearch($search);
 }
